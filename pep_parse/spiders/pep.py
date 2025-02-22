2b46dd86710367fc617d4c4b61678b7e5ca1b1d6
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-22 19:24:12 +0300

    Make list with pep-links

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,4 +1,7 @@
+from urllib.parse import urljoin
+
 import scrapy
+from scrapy import Selector
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
@@ -6,10 +9,22 @@ class PepSpider(scrapy.Spider):
     start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        print('=================================================================================================')
-        print('|'*100)
-        print(response)
-        print('|'*100)
-        print('|'*100)
+        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+        # shortlinks = response.xpath('//table[contains(@class, "pep-zero-table")]/tbody/tr/td[3]/a').xpath('@href')
+
+        links = [
+            urljoin(self.start_urls[0], shortlink.get()) for shortlink in (
+                response.xpath('//table[contains(@class, "pep-zero-table")]'
+                               '/tbody/tr/td[3]/a').xpath('@href')
+            )
+        ]
+
+
+        for link in links:
+            print(link)
+        # for pep_shortlink in pep_shortlinks:
+        #     # print(self.start_urls[0], pep_shortlink.get())
+        #     pep_link = urljoin(self.start_urls[0], pep_shortlink.get())
+        #     print(pep_link)
 
 
