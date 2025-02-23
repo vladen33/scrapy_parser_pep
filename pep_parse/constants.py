1b25afa9ed7af793c654eaa5dc46e4d6db93e743
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-23 14:57:29 +0300

    Final 3

---
+++ pep_parse/constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+RESULTS_DIR = 'results'
+
+DATE_FORMAT = '%Y-%m-%d_%H-%M-%S'
--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,14 +1,11 @@
 import datetime as dt
 from collections import defaultdict
 
+from pep_parse.constants import BASE_DIR, DATE_FORMAT, RESULTS_DIR
+
 
 class PepParsePipeline:
     def open_spider(self, spider):
-        now = dt.datetime.now()
-        now_formatted = now.strftime('%Y-%m-%d_%H-%M-%S')
-        self.file = open(
-            'results/status_summary_' + now_formatted, 'w', encoding='utf-8')
-        self.file.write('"Статус", "Количество"\n')
         self.status_counter = defaultdict(int)
 
     def process_item(self, item, spider):
@@ -16,7 +13,13 @@ class PepParsePipeline:
         return item
 
     def close_spider(self, spider):
-        for key in self.status_counter:
-            self.file.write(f'{key}, {self.status_counter[key]}\n')
-        self.file.write(f'ИТОГО, {sum(self.status_counter.values())}')
-        self.file.close()
+        now_formatted = dt.datetime.now().strftime(DATE_FORMAT)
+        results_dir = BASE_DIR / RESULTS_DIR
+        results_dir.mkdir(exist_ok=True)
+        file_name = f'status_summary_{now_formatted}.csv'
+        file_path = results_dir / file_name
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write('"Статус", "Количество"\n')
+            for key in self.status_counter:
+                file.write(f'{key}, {self.status_counter[key]}\n')
+            file.write(f'ИТОГО, {sum(self.status_counter.values())}')
