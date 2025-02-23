fb16ed6987134c902cca6da57367d610591ccc30
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-23 12:19:58 +0300

    make file 1

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -4,6 +4,8 @@ from urllib.parse import urljoin
 import scrapy
 from scrapy import Selector
 
+from pep_parse.items import PepParseItem
+
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
@@ -11,32 +13,21 @@ class PepSpider(scrapy.Spider):
 
     def parse(self, response):
         shortlinks = response.xpath('//table[contains(@class, "pep-zero-table")]/tbody/tr/td[3]/a').xpath('@href')
-
-        counter = 1
+        cnt = 1
         for link in shortlinks:
-            counter += 1
-            if counter > 5:
+            cnt += 1
+            if cnt > 10:
                 break
             yield response.follow(link, callback=self.parse_pep)
-            # print(link)
+
 
 
     def parse_pep(self, response):
         pep_title = response.css('h1.page-title::text').get()
         result = re.fullmatch(r'PEP (?P<number>\d{1,4}) – (?P<name>.*)', pep_title)
-        number = result.group('number')
-        name = result.group('name')
-        status = response.xpath('//abbr[1]/text()').get()
-
-        print('NUMBER = ', result.group('number'))
-        print('NAME = ', result.group('name'))
-        print('STATUS = ', status)
-
-        # print('>>=='*20, res.groups())
-
-
-
-
-        # yield {'aaa': 'AAAAAA'}
-
-
+        data = {
+            'number': result.group('number'),
+            'name': result.group('name'),
+            'status': response.xpath('//abbr[1]/text()').get()
+        }
+        yield PepParseItem(data)
